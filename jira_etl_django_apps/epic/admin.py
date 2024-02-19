4d6835d4e242a300fa348111a32f6388f6fb9921
renneralexandre <renner.alexandre@gmail.com>
2024-02-18 23:05:11 -0300

    fix: add css to Django Admin

--- jira_etl_django_apps/epic/admin.py
+++ jira_etl_django_apps/epic/admin.py
@@ -3,4 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from .models import Epic
 
-admin.site.register(Epic)
+
+class EpicAdmin(admin.ModelAdmin):
+    list_display = ('jiraProject', 'jiraKey', 'jiraStatus', 'jiraEpicName', 'jiraAssignee', 'jiraCreatedDate', 'classification')
+    class Media:
+        css = {
+             'all': ["epic/x.css"]
+             
+        }
+
+admin.site.register(Epic, EpicAdmin)
